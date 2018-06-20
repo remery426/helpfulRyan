8239b2779a37cff9ecdc721743aaa03479a0a1f6
remery426 <emeryryan1@gmail.com>
2018-06-19 23:25:34 -0700

    initial commit

--- src/components/Detailview/Detailview.js
+++ src/components/Detailview/Detailview.js
@@ -3,7 +3,7 @@ import Button from '../Sidebar/Button';
 const Detailview = (props)=>{
   return(
     <div>
-    <div style = {{border: '1px solid grey',marginLeft:'21%', width:'75%', min-height:600, display:'block'}}>
+    <div style = {{border: '1px solid grey',marginLeft:'21%', width:'75%', minHeight:600, display:'block'}}>
     <div style = {{width:'100%', height:'80%',backgroundColor:props.detailColor}}></div>
     <h1> {props.detailColor} </h1>
   </div>
