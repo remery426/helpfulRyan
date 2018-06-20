4ceef798cdc86ac1e589f73895f5af18655acda2
remery426 <emeryryan1@gmail.com>
2018-06-20 00:31:14 -0700

    initial commit

--- src/components/ListSquare.js
+++ src/components/ListSquare.js
@@ -3,7 +3,7 @@ import '../List.css';
 
 const ListSquare = (props)=>{
   return(
-    <div style={{paddingLeft:'10%'}} onClick = {()=>props.onClick(props.children)}>
+    <div style={{display:'inlineBlock', paddingLeft:'10%'}} onClick = {()=>props.onClick(props.children)}>
       <div style = {{backgroundColor:props.children, width:150,height:150}}></div>
       <p style ={{textAlign:'left',color:props.children}}> {props.children}</p>
     </div>
